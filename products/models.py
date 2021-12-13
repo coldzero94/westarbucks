001894bf467ab8ee648f0a90dc3fa6828d20606d
chanyoung <cyl0504@gmail.com>
2021-12-13 16:16:48 +0900

    Add: models more detail

--- products/models.py
+++ products/models.py
@@ -1,44 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=45)
 
-class Category(models.Model):
-	name = models.CharField(max_length=50)
+class Categories(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
 
-	class meta:
-		db_table = 'categories'
-
-class SubCategory(models.Model):
-	name = models.CharField(max_length=50)
-	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True)
-
-	class meta:
-		db_table = 'sub_categories'
-
-class Drink(models.Model):
-	name = models.CharField(max_length=100)
-	description = models.TextField(max_length=3000)
-	thumbnail = models.URLField(max_length=500)
-	serving = models.DecimalField(max_digits=4, decimal_places=1)
-	claories = models.DecimalField(max_digits=5, decimal_places=1)
-	saturated_fat = models.DecimalField(max_digits=4, decimal_places=1)
-	protein = models.DecimalField(max_digits=4, decimal_places=1)
-	sodium = models.DecimalField(max_digits=4, decimal_places=1)
-	sugar = models.DecimalField(max_digits=4, decimal_places=1)
-	caffein = models.DecimalField(max_digits=4, decimal_places=1)
-	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-	sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True)
-	allergy = models.ManyToManyField('Allergy', through= 'DrinkAllergy')
-
-	class meta:
-		db_table = 'drinks'
+class Drinks(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    kr_name = models.CharField(max_length=45)
+    en_name = models.CharField(max_length=45)
+    description = models.TextField()
 
 class Allergy(models.Model):
-	name = models.CharField(max_length=50)
+    name = models.CharField(max_length=45)
 
-class DrinkAllergy(models.Model):
-	drink = models.ForeignKey('Drink', on_delete=models.SET_NULL, null=True)
-	allergy = models.ForeignKey('Allergy', on_delete=models.SET_NULL, null=True)
+class Allergy_drink(models.Model):
+    allergy = models.ForeignKey(Allergy, on_delete=models.CASCADE)
+    drink = models.ForeignKey(Drinks, on_delete=models.CASCADE)
 
-	class meta:
-		db_table = 'drink_allergies'
+class Images(models.Model):
+    image_url = models.CharField(max_length=2000)
+    drink = models.ForeignKey(Drinks, on_delete=models.CASCADE)
+
+class Sizes(models.Model):
+    name = models.CharField(max_length=45)
+    size_ml = models.CharField(max_length=45, null=True)
+    size_fluid_ounce = models.CharField(max_length=45, null=True)
+
+class Nutritions(models.Model):
+    one_serving_kcal = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    sodium_mg = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    saturated_fat_g = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    sugars_g = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    protein_g = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    caffeine_mg = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    drink = models.ForeignKey(Drinks, on_delete=models.SET_NULL, null=True)
+    size = models.ForeignKey(Sizes, on_delete=models.SET_NULL, null=True)
